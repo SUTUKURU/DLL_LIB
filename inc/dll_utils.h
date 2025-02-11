d5671ae888c686f6ed76e04355a8125f47489366
subramanyam <utukurus1990@gmail.com>
2025-02-11 18:52:52 +0000

    updating files with dll apis declarations and structs

--- inc/dll_utils.h
+++ inc/dll_utils.h
@@ -1,3 +1,24 @@
 /* File - dll_utils.h
    Author - Subramanyam Utukuru
  */
+
+#ifndef __DLL_UTILS_H__
+#define __DLL_UTILS_H__
+
+typedef struct dll_node_ {
+    void *data;
+    struct dll_node_ *left;
+    struct dll_node_ *right;
+}dll_node_t;
+
+typedef struct dll_ {
+    dll_node_t *head;
+}dll_t;
+
+dll_t* get_new_dll();
+int add_data_to_dll(dll_t *dll, void *data);
+int remove_data_from_dll_by_data_ptr(dll_t *dll, void *data);
+int is_dll_empty(dll_t *dll);
+void drain_dll(dll_t *dll);
+
+#endif __DLL_UTILS_H__
--- src/dll_utils.c
+++ src/dll_utils.c
@@ -1,4 +1,40 @@
-/* File - dll_lib.c
+/* File - dll_utils.c
    Author - Subramanyam Utukuru
    Implementing generic double linked list library APIs to use by many applications.
- */ 
+ */
+
+/* Create and Return new DLL */
+dll_t* 
+get_new_dll() {
+
+
+}
+
+/* Add application data to dll */
+int
+add_data_to_dll(dll_t *dll, void *data) {
+
+
+}
+
+/* remove node from dll based on app data 
+   return 0 on success
+   -1 on failure*/
+int
+remove_data_from_dll_by_data_ptr(dll_t *dll, void *data) {
+
+}
+
+
+/* Find is dll empty.
+   return  0 - if empty
+   return -1 if non-empty */
+int
+is_dll_empty(dll_t *dll) {
+
+}
+
+/* delete all nodes from a dll, but do not free app data*/
+void drain_dll(dll_t *dll) {
+
+} 
